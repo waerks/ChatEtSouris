457e4ee857fa3428f460eb4195e36f772f0b9ccb
Anaïs <a.aerts@interface3.be>
2025-01-06 16:21:24 +0100

    Souris Deplacer()

--- ChatEtSouris/Models/Souris.cs
+++ ChatEtSouris/Models/Souris.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,19 @@ namespace ChatEtSouris.Models
 	{
 		public int PositionX;
 		public int PositionY;
+		private Random _random;
+
+		public void Deplacer()
+		{
+			_random = new Random();
+
+			int deplacementX = _random.Next(-1,2);
+			int deplacementY = _random.Next(-1,2);
+
+			PositionX += deplacementX;
+			PositionY += deplacementY;
+
+            Console.WriteLine($"La souris se déplace en ({PositionX}, {PositionY})");
+		}
 	}
 }
--- ChatEtSouris/Program.cs
+++ ChatEtSouris/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace ChatEtSouris
+﻿using ChatEtSouris.Models;
+
+namespace ChatEtSouris
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Souris souris = new Souris();
+			souris.PositionX = 0;
+			souris.PositionY = 0;
+			souris.Deplacer();
 		}
 	}
 }
